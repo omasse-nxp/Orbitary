196c02ad00ca8dab785284ef2234cacb348ca81d
omasse-nxp <olivier.masse@nxp.com>
2026-04-14 21:42:15 +0200

    Add simple collision
    Signed-off-by: omasse-nxp <olivier.masse@nxp.com>

--- ship_7.gd
+++ ship_7.gd
@@ -1,8 +1,8 @@
 extends RigidBody3D
 
-var ship_thrust = 400
+var ship_thrust = 9000
 var ship_z_thrust = 0
-var ship_rot_thrust = 200
+var ship_rot_thrust = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,10 +22,10 @@ func _physics_process(delta):
 	
 	apply_central_force(transform.basis.z * ship_z_thrust * delta)
 	if Input.is_action_just_pressed("ui_left"):
-		apply_torque_impulse(transform.basis.y * -ship_rot_thrust * delta)
+		apply_torque_impulse(transform.basis.y * +ship_rot_thrust * delta)
 
 	if Input.is_action_just_pressed("ui_right"):
-		apply_torque_impulse(transform.basis.y * +ship_rot_thrust * delta)
+		apply_torque_impulse(transform.basis.y * -ship_rot_thrust * delta)
 		
 	if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
 		angular_velocity = Vector3.ZERO
